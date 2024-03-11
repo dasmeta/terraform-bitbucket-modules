e3b0c2c78eb021e49d84173583918ad0da0e1ab5
Aram Karapetyan <aram@dasmeta.com>
2024-03-11 10:04:44 +0400

    fix(DMVP-test): Change to deployment

--- modules/runner/job.tf
+++ modules/runner/job.tf
@@ -5,107 +5,113 @@ resource "kubernetes_deployment" "runner" {
     namespace = var.namespace
   }
 
-  spec {
-    template {
-      metadata {
-        labels = {
-          accountUuid = var.accountUuid
-          runnerUuid  = var.runnerUuid
-        }
-      }
-      spec {
-        container {
-          name  = "bitbucket-k8s-runner"
-          image = "docker-public.packages.atlassian.com/sox/atlassian/bitbucket-pipelines-runner"
+  selector {
+    match_labels = {
+      test = "runner"
+    }
+  }
 
-          env {
-            name  = "ACCOUNT_UUID"
-            value = "{${var.accountUuid}}"
-          }
-
-          env {
-            name  = "RUNNER_UUID"
-            value = "{${var.runnerUuid}}"
-          }
-
-          env {
-            name = "OAUTH_CLIENT_ID"
-            value_from {
-              secret_key_ref {
-                name = "runner-oauth-credentials"
-                key  = "oauthClientId"
-              }
-            }
-          }
-
-          env {
-            name = "OAUTH_CLIENT_SECRET"
-            value_from {
-              secret_key_ref {
-                name = "runner-oauth-credentials"
-                key  = "oauthClientSecret"
-              }
-            }
-          }
-
-          env {
-            name  = "WORKING_DIRECTORY"
-            value = "/tmp"
-          }
-
-          volume_mount {
-            name       = "tmp"
-            mount_path = "/tmp"
-          }
-
-          volume_mount {
-            name       = "docker-containers"
-            mount_path = "/var/lib/docker/containers"
-            read_only  = true
-          }
-
-          volume_mount {
-            name       = "var-run"
-            mount_path = "/var/run"
-          }
-        }
-
-        container {
-          name  = "docker-in-docker"
-          image = "docker:20.10.7-dind"
-          security_context {
-            privileged = true
-          }
-
-          volume_mount {
-            name       = "tmp"
-            mount_path = "/tmp"
-          }
-
-          volume_mount {
-            name       = "docker-containers"
-            mount_path = "/var/lib/docker/containers"
-          }
-
-          volume_mount {
-            name       = "var-run"
-            mount_path = "/var/run"
-          }
-        }
-        restart_policy = "OnFailure"
-        volume {
-          name = "tmp"
-        }
-
-        volume {
-          name = "docker-containers"
-        }
-
-        volume {
-          name = "var-run"
-        }
+  template {
+    metadata {
+      labels = {
+        accountUuid = var.accountUuid
+        runnerUuid  = var.runnerUuid
       }
     }
+
+    spec {
+      container {
+        name  = "bitbucket-k8s-runner"
+        image = "docker-public.packages.atlassian.com/sox/atlassian/bitbucket-pipelines-runner"
+
+        env {
+          name  = "ACCOUNT_UUID"
+          value = "{${var.accountUuid}}"
+        }
+
+        env {
+          name  = "RUNNER_UUID"
+          value = "{${var.runnerUuid}}"
+        }
+
+        env {
+          name = "OAUTH_CLIENT_ID"
+          value_from {
+            secret_key_ref {
+              name = "runner-oauth-credentials"
+              key  = "oauthClientId"
+            }
+          }
+        }
+
+        env {
+          name = "OAUTH_CLIENT_SECRET"
+          value_from {
+            secret_key_ref {
+              name = "runner-oauth-credentials"
+              key  = "oauthClientSecret"
+            }
+          }
+        }
+
+        env {
+          name  = "WORKING_DIRECTORY"
+          value = "/tmp"
+        }
+
+        volume_mount {
+          name       = "tmp"
+          mount_path = "/tmp"
+        }
+
+        volume_mount {
+          name       = "docker-containers"
+          mount_path = "/var/lib/docker/containers"
+          read_only  = true
+        }
+
+        volume_mount {
+          name       = "var-run"
+          mount_path = "/var/run"
+        }
+      }
+
+      container {
+        name  = "docker-in-docker"
+        image = "docker:20.10.7-dind"
+        security_context {
+          privileged = true
+        }
+
+        volume_mount {
+          name       = "tmp"
+          mount_path = "/tmp"
+        }
+
+        volume_mount {
+          name       = "docker-containers"
+          mount_path = "/var/lib/docker/containers"
+        }
+
+        volume_mount {
+          name       = "var-run"
+          mount_path = "/var/run"
+        }
+      }
+      restart_policy = "Always"
+      volume {
+        name = "tmp"
+      }
+
+      volume {
+        name = "docker-containers"
+      }
+
+      volume {
+        name = "var-run"
+      }
+    }
+
 
     # backoff_limit = 6
     # completions   = 1
