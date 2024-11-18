62a9f797caa884b396bf1257980963d1d0616ed7
Sarhad <sarhad@dasmeta.com>
2024-11-18 15:21:19 +0400

    fix(DMVP-5941): cmd change

--- modules/runner/cronjob.tf
+++ modules/runner/cronjob.tf
@@ -25,9 +25,8 @@ resource "kubernetes_cron_job_v1" "runner_cronjob" {
 
           spec {
             container {
-              name    = "bitbucket-k8s-runner"
-              image   = "docker-public.packages.atlassian.com/sox/atlassian/bitbucket-pipelines-runner"
-              command = ["/bin/sh", "-c", "echo ACCOUNT_UUID: $ACCOUNT_UUID && echo RUNNER_UUID: $RUNNER_UUID"]
+              name  = "bitbucket-k8s-runner"
+              image = "docker-public.packages.atlassian.com/sox/atlassian/bitbucket-pipelines-runner"
 
               env {
                 name  = "ACCOUNT_UUID"
